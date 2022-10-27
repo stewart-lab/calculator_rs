0494b5c954496328bb0b06d83da62a087fc95696
Ron Stewart <rstewart@morgridge.org>
2022-10-27 15:12:06 -0500

    commit a bugg

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    print('hello calculator')
+    a = 2
+    b = 4
+    c  = multiply(a,b)
+    print(str(a) + " * " + str(b) + ' = ' + str(c))
+    
+def multiply(x,y):
+    # do it wrong 
+    return x / y
+    
+if __name__ == '__main__':
+    main()
---
+++ test_calc.py
@@ -0,0 +1,4 @@
+def test_multiply():
+    import main
+    result = main.multiply(3,5)
+    assert(result == 15)
